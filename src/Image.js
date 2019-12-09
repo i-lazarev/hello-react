660bc4c719eb862d9dfa6699bfe1e6724dead75b
i-lazarev <vanealazarev@gmail.com>
2019-12-09 14:53:06 +0100

    ...

---
+++ src/Image.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import Message from "./Message";
+
+export default function Image() {
+  return (
+    <Fragment>
+      <p>Here I am</p>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Message  user="Leandro" age="42"/>
+      <Message  user="Tareq" age="17"/>
+      <Message user="Me"  age="30"/>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </Fragment>
+  );
+}
---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Message(props) {
+    console.log(props)
+    const message = `Hi ${props.user} wellcome`
+  return (
+    <div>
+<p>{message} you are  {props.age}</p>
+    </div>
+  );
+}
