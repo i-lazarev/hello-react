517c0f6785e874b3bceab7d1f15a86e0e9333eba
i-lazarev <vanealazarev@gmail.com>
2019-12-10 10:18:59 +0100

    ...

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./App.css";
 import Image from "./Image";
 
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Image />
-       
       </header>
     </div>
   );
 }
 
-export default App;
+
