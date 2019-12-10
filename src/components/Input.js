ce9601da96426ee4c26253df3371197d5d82dd6f
i-lazarev <vanealazarev@gmail.com>
2019-12-10 11:55:16 +0100

    ...

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,13 +3,27 @@ import Message from './Message'
 
 export default class Input extends Component {
 
+    // constructor(props) {
+    //     super(props);
+    //     // hard binding -> of this.updateUser
+    //     // this.updateUser = this.updateUser.bind(this)
+    // }
+
     updateUser = (event) => {
+        console.log(this)
         //we want to update the state with the event.target.value
         this.setState({
-            user: event.target.value,
-            age: 'too'
+            user: event.target.value
         })
     }
+
+    updateAge = (event) => {
+        //we want to update the state with the event.target.value
+        this.setState({
+            age: event.target.value
+        })
+    }
+
 
     render() {
         return (
@@ -18,9 +32,16 @@ export default class Input extends Component {
                     {!this.state &&
                         "Type something please (to set a state)"
                     }</p>
+                <span>User:</span>
                 <input type="text" id="user"
                     onChange={this.updateUser}
-                    placeholder={this.state && this.state.user} />
+                />
+
+                <span>Age:</span>
+                <input type="number" id="age"
+                    onChange={this.updateAge}
+                />
+
                 {/* Conditional Rendering */}
                 {this.state &&
                     <Message user={this.state.user} age={this.state.age} />
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from 'react'
 
 export default function Message(props) {
     console.log(props)
-    const message = `Hi ${props.user} wellcome`
-  return (
-    <div>
-<p>{message} you are  {props.age}</p>
-
-    </div>
-  );
+    const message = `, you're ${props.age}  years old`
+    return (
+        <div>
+            Hi {props.user}
+            {props.age &&
+                message
+            }
+        </div>
+    )
 }
